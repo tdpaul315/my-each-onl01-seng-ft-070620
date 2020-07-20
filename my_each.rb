e1038e99a0c6f46c827d39250760b2f40e6bcf54
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-20 05:50:06 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,11 @@
-list = ['arel', 'jon', 'logan', 'spencer']
-
 def my_each(array)
   i = 0
   while i < array.length
     yield(array[i])
-    i = i + 1
+    i += 1
   end
+  array
 end
-
-my_each(list) {|i|}
 
 
 
