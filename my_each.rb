41c8f618b98932755fa869388f45b47859ec0034
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-20 05:44:45 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,23 @@
-def my_each # put argument(s) here
-  # code here
+list = ['arel', 'jon', 'logan', 'spencer']
+
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
 end
+
+my_each(list) {|i|}
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i = i + 1 
+  end
+  collection
+end
+
+
